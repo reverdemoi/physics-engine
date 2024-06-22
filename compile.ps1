568846e07e0822b2e2ca06ace08d736f0b8fcd57
Albin Söderberg <albin@nis.nu>
2024-06-23 00:46:57 +0200

    updated compile script to take multiple files into account

--- compile.ps1
+++ compile.ps1
@@ -1,14 +1,25 @@
 # compile.ps1
-$includeDir = ".\include"
+$includeDir = ".\headers"
+$includeSDLDir = ".\include"
 $libDir = ".\lib"
 $fileName = "main"
 
-gcc -o main main.c -IC:$includeDir -LC:$libDir -lmingw32 -lSDL2main -lSDL2
+# List of source files
+$sourceFiles = @(
+    ".\source\main.c",
+    ".\source\vector.c",
+    ".\source\SDL_utils.c",
+    ".\source\ball.c",
+    ".\source\common.c"
+)
+
+# Compile the source files
+gcc -o $fileName $sourceFiles -I"$includeDir" -I"$includeSDLDir" -L"$libDir" -lmingw32 -lSDL2main -lSDL2 -lm
 
 # Check if the gcc command was successful
 if ($LASTEXITCODE -eq 0) {
-    Write-Output "Compilation successful. Running window.exe..."
-    .\main.exe
+    Write-Output "Compilation successful. Running $fileName.exe..."
+    & ".\$fileName.exe"
 } else {
-    Write-Output "Compilation failed. window.exe will not be run."
+    Write-Output "Compilation failed. $fileName.exe will not be run."
 }
