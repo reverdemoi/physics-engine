4f036889b8b9091067bd9fff808282a890a03a61
Albin Söderberg <albin@nis.nu>
2024-06-23 18:30:36 +0200

    added FPS counter

--- source/main.c
+++ source/main.c
@@ -19,27 +19,43 @@
 5. after a while the balls get so much momentum they wont stop
 */
 
+Ball* initBalls(Ball* borderBall) {
+    Ball* balls;
+
+    *borderBall = (Ball){{SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2}, {0, 0}, 400};
+    
+    balls = (Ball*)malloc(sizeof(Ball) * numBalls);
+    if (balls == NULL) {
+        printf("Failure to allocate memory\n");
+        return NULL;
+    }
+
+    generateBalls(numBalls, balls, borderBall);
+    srand(time(NULL));
+
+    return balls;
+}
 
 int main(int argc, char* argv[]) {
     bool exit = false;
     SDL_Event e;
     Uint32 lastTime = SDL_GetTicks();
+    Uint32 frameStart;
+    Uint32 frameTime;
+    int frameCount = 0;
+    float fps = 0.0f;
 
     if (!init()) {
         printf("Failure to initialize\n");
     } else {
-        Ball borderBall = {{SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2}, {0, 0}, 400};
+        Ball borderBall;
+        Ball* balls;
 
-        Ball* balls = (Ball*)malloc(sizeof(Ball) * numBalls);
-        if (balls == NULL) {
-            printf("Failure to allocate memory\n");
-            return 1;
-        }
-
-        srand(time(NULL));
-        generateBalls(numBalls, balls, &borderBall);
+        balls = initBalls(&borderBall);
 
         while (!exit) {
+            frameStart = SDL_GetTicks();
+
             // make background black
             SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
             SDL_RenderClear(renderer);
@@ -55,12 +71,13 @@ int main(int argc, char* argv[]) {
 
                 if (e.type == SDL_MOUSEBUTTONDOWN) {
                     // DEBUG - teleport ball to mouse position
-                    int mouseX, mouseY;
-                    Uint32 mouseState = SDL_GetMouseState(&mouseX, &mouseY);
-                    balls[0].position.x = mouseX;
-                    balls[0].position.y = mouseY;
-                    balls[0].velocity.y = 2;
-                    balls[0].velocity.x = 1;
+                    // int mouseX, mouseY;
+                    // Uint32 mouseState = SDL_GetMouseState(&mouseX, &mouseY);
+                    // balls[0].position.x = mouseX;
+                    // balls[0].position.y = mouseY;
+                    // balls[0].velocity.y = 2;
+                    // balls[0].velocity.x = 1;
+                    balls = initBalls(&borderBall);
                 }
             }
 
@@ -90,6 +107,10 @@ int main(int argc, char* argv[]) {
                 // Handle ball[i] going outside border
                 handleOutOfBounds(&balls[i], &borderBall);
 
+                handleBallCollision(&balls[i], balls, numBalls, &borderBall);
+                // printf("ball[0] velocity: %f, %f\n", balls[0].velocity.x, balls[0].velocity.y);
+                // printf("ball[0] magnitude of velocity: %f\n", magnitude(balls[0].velocity));
+
                 // Update ball[i] position
                 balls[i].position = add(balls[i].position, multiply(balls[i].velocity, TIME_STEP));
 
@@ -97,9 +118,6 @@ int main(int argc, char* argv[]) {
 
                 // printf("RUNNING MAIN FOR LOOP ITERATION %d\n", i);
 
-                handleBallCollision(&balls[i], balls, numBalls, &borderBall);
-                printf("ball[0] velocity: %f, %f\n", balls[0].velocity.x, balls[0].velocity.y);
-                printf("ball[0] magnitude of velocity: %f\n", magnitude(balls[0].velocity));
 
                 ////////////////////////// IF MAGNITUDE OF VELOCITY IS LESS THAN 0.1 THEN CANCEL GRAVITY, DO THAT BY ADDING GRAVITY PROPERTY TO BALL AND ONLY ADDING GRAVITY IF THAT PROPERTY IS TRUE
 
@@ -109,6 +127,16 @@ int main(int argc, char* argv[]) {
 
             // Update screen
             SDL_RenderPresent(renderer);
+
+            frameTime = SDL_GetTicks() - frameStart;
+            frameCount++;
+            if (frameTime > 0) {
+                fps = 1000.0f / frameTime;
+            }
+
+            if (frameCount % 60 == 0) {
+                printf("FPS: %f\n", fps);
+            }
 
             // Delay for approx. 30 fps
             SDL_Delay(16) ; // 32 ms
