2a97af0431144f413412de9d1a57615d19240221
Albin Söderberg <albin@nis.nu>
2024-06-23 18:29:32 +0200

    fixed bug where balls would overlap

--- source/ball.c
+++ source/ball.c
@@ -6,13 +6,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+const int pi = 3.1415926535897932;
+
 void generateBalls(int numBalls, Ball* balls, Ball* borderBall) {
     double centerX = SCREEN_WIDTH / 2.0;
     double centerY = SCREEN_HEIGHT / 2.0;
 
     for (int i = 0; i < numBalls; i++) {
         // Generate random point inside the circle
-        double angle = ((double)rand() / RAND_MAX) * 2.0 * M_PI;
+        double angle = ((double)rand() / RAND_MAX) * 2.0 * pi;
         double radius = sqrt((double)rand() / RAND_MAX) * borderBall->radius; // sqrt for uniform distribution
 
         balls[i].position.x = centerX + radius * cos(angle);
@@ -21,14 +23,14 @@ void generateBalls(int numBalls, Ball* balls, Ball* borderBall) {
         // Random velocity and radius
         balls[i].velocity.x = ((double)rand() / RAND_MAX) * 10.0 + 1.0;
         balls[i].velocity.y = ((double)rand() / RAND_MAX) * 10.0 + 1.0;
-        balls[i].radius = ((double)rand() / RAND_MAX) * 20.0 + 10.0;
+        balls[i].radius = ((double)rand() / RAND_MAX) * 5.0 + 5.0;
         balls[i].gravity = true;
     }
 
     // print all values od balls
-    for (int i = 0; i < numBalls; i++) {
-        printf("Ball %d: posX: %f, posY: %f, velX: %f, velY: %f, radius: %f\n", i, balls[i].position.x, balls[i].position.y, balls[i].velocity.x, balls[i].velocity.y, balls[i].radius);
-    }
+    // for (int i = 0; i < numBalls; i++) {
+    //     printf("Ball %d: posX: %f, posY: %f, velX: %f, velY: %f, radius: %f\n", i, balls[i].position.x, balls[i].position.y, balls[i].velocity.x, balls[i].velocity.y, balls[i].radius);
+    // }
 }
 
 double borderCollision(Ball* ball, Ball* borderBall) {
@@ -114,45 +116,19 @@ void handleBallCollision(Ball* ball, Ball* balls, int numBalls, Ball* borderBall
         }
 
         if (collisionCheck(ball, &balls[j])) {
-            printf("BALL COLLISION\n");
+            // printf("BALL COLLISION\n");
 
             Vector newVel1, newVel2;
             calculateVelocities(ball->radius, ball->velocity, balls[j].radius, balls[j].velocity, &newVel1, &newVel2, ball->position, balls[j].position);
 
-            // check if stuck inside
-            Vector normalVel1 = normalize(newVel1);
-            Vector normalVel2 = normalize(newVel2);
+            /* STAY OUTSIDE OF EACH OTHER */
+            Vector collisionNormal = normalize(subtract(ball->position, balls[j].position));
+            double overlap = ball->radius + balls[j].radius - magnitude(subtract(ball->position, balls[j].position));
 
-            Vector newPos1 = add(ball->position, multiply(newVel1, TIME_STEP)); 
-            Vector newPos2 = add(balls[j].position, multiply(newVel2, TIME_STEP));
+            ball->position = add(ball->position, multiply(collisionNormal, overlap / 2.0)); 
+            balls[j].position = subtract(balls[j].position, multiply(collisionNormal, overlap / 2.0));
 
-            Ball newBall1 = {newPos1, newVel1, ball->radius};
-            Ball newBall2 = {newPos2, newVel2, balls[j].radius};
-
-            if (collisionCheck(&newBall1, &newBall2)) {
-                printf("BALL STUCK\n");
-                // printf("BALL1: magnitude of velocity: %f\n", magnitude(newVel1));
-                // printf("BALL1: normal of velocity, x: %f, y: %f\n", normalVel1.x, normalVel1.y);
-                // printf("BALL2: magnitude of velocity: %f\n", magnitude(newVel2));
-                // printf("BALL2: normal of velocity, x: %f, y: %f\n", normalVel2.x, normalVel2.y);
-                // ball->velocity = multiply(ball->velocity, -1);
-                // balls[j].velocity = multiply(balls[j].velocity, -1);
-                return;
-            }
-
-            // Check if still inside
-            // distanceVec = subtract(ball->position, balls[j].position);
-            // distance = magnitude(distanceVec);
-
-            //     if (distance <= (ball->radius + balls[j].radius)) {
-            //         double angle = ((double)rand() / RAND_MAX) * 2.0 * M_PI;
-            //         double radius = sqrt((double)rand() / RAND_MAX) * borderBall->radius; // sqrt for uniform distribution
-
-            //         ball->position.x = borderBall->position.x + radius * cos(angle);
-            //         ball->position.y = borderBall->position.y + radius * sin(angle);
-            //   }
-
-
+            /* UPDATE VELOCITIES */
             ball->velocity = newVel1;
             balls[j].velocity = newVel2;
 
