75d135eaedc7a6b86cbfba4a4bdfaaf3714c78ea
Albin Söderberg <albin@nis.nu>
2024-06-23 19:55:09 +0200

    did something

--- source/common.c
+++ source/common.c
@@ -7,4 +7,4 @@ double TIME_STEP = 0.1;
 const double VELOCITY_MODIFIER = 0.8;
 double GRAVITY = 9.8;
 
-int numBalls = 200;
+// int numBalls = 100;
--- vault.c
+++ vault.c
@@ -43,3 +43,46 @@
             // Uint32 mouseState = SDL_GetMouseState(&mouseX, &mouseY);
             // circleX = mouseX;
             // circleY = mouseY;
+
+
+
+
+
+
+
+
+            
+            // check if stuck inside 
+            /* 
+            Vector normalVel1 = normalize(newVel1);
+            Vector normalVel2 = normalize(newVel2);
+
+            Vector newPos1 = add(ball->position, multiply(newVel1, TIME_STEP)); 
+            Vector newPos2 = add(balls[j].position, multiply(newVel2, TIME_STEP));
+
+            Ball newBall1 = {newPos1, newVel1, ball->radius};
+            Ball newBall2 = {newPos2, newVel2, balls[j].radius};
+
+            if (collisionCheck(&newBall1, &newBall2)) {
+                // printf("BALL STUCK\n");
+                // printf("BALL1: magnitude of velocity: %f\n", magnitude(newVel1));
+                // printf("BALL1: normal of velocity, x: %f, y: %f\n", normalVel1.x, normalVel1.y);
+                // printf("BALL2: magnitude of velocity: %f\n", magnitude(newVel2));
+                // printf("BALL2: normal of velocity, x: %f, y: %f\n", normalVel2.x, normalVel2.y);
+                // ball->velocity = multiply(ball->velocity, -1);
+                // balls[j].velocity = multiply(balls[j].velocity, -1);
+                return;
+            }
+
+            // Check if still inside
+            // distanceVec = subtract(ball->position, balls[j].position);
+            // distance = magnitude(distanceVec);
+
+            //     if (distance <= (ball->radius + balls[j].radius)) {
+            //         double angle = ((double)rand() / RAND_MAX) * 2.0 * M_PI;
+            //         double radius = sqrt((double)rand() / RAND_MAX) * borderBall->radius; // sqrt for uniform distribution
+
+            //         ball->position.x = borderBall->position.x + radius * cos(angle);
+            //         ball->position.y = borderBall->position.y + radius * sin(angle);
+            //   } 
+            */
